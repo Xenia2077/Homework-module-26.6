9d2a559de237c9b66847e91a45d4726e358f3a3d
Ксения Борисова <freuleingast@gmail.com>
2022-03-14 22:30:03 +0300

    Создала приложение, сохраняющее настройки темы при выходе.

--- AppWithBackground/ViewController.swift
+++ AppWithBackground/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    let ourDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        themeSwitch.isOn = ourDefaults.bool(forKey: "savedTheme")
+        toggleThemeSwitch()
+    }
+    
+    func toggleThemeSwitch() {
+        if themeSwitch.isOn {
+            myImageView.image = UIImage(named: "pic4")
+            view.backgroundColor = .darkGray
+            ourDefaults.set(themeSwitch.isOn, forKey: "savedTheme")
+        } else {
+            myImageView.image = UIImage(named: "pic6")
+            view.backgroundColor = .systemGray5
+            ourDefaults.set(themeSwitch.isOn, forKey: "savedTheme")
+        }
     }
 
 
+    @IBAction func themeSwitchPressed(_ sender: UISwitch) {
+        toggleThemeSwitch()
+    }
+    
 }
 
